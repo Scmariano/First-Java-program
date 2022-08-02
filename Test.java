01bc35d3c2e11d4b2e82666491b42bf1c8d249f7
Scmariano <104543069+Scmariano@users.noreply.github.com>
2022-08-01 20:28:24 -0700

    commit

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("My name is Stephen");
+        System.out.println("I am 33 years old");
+        System.out.println("My hometown is in Los Angeles, CA");
+    }
+}
